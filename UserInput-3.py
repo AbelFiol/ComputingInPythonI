bf2d60bc2dc798e5c68d73a7d8f0a65ecbd8ea1b
AbelFiol <abelfiol2712@gmail.com>
2025-07-30 11:37:12 -0500

    User Input 3

---
+++ UserInput-3.py
@@ -0,0 +1,3 @@
+myUserInput = input("Enter an integer: ")          # Prompt the user to enter an integer (input is a string)
+myUserInputAsInt = int(myUserInput)                # Convert the input string to an integer
+print(myUserInputAsInt * myUserInputAsInt)         # Print the square of the integer
