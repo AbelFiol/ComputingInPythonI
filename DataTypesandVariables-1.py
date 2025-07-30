692c3e6d2c5e23a0fbd1a0cb4e1e4d385c2f87c6
AbelFiol <abelfiol2712@gmail.com>
2025-07-30 10:58:08 -0500

    Data Types and Variables 1

---
+++ DataTypesandVariables-1.py
@@ -0,0 +1,5 @@
+from datetime import date  # Import the date class from the datetime module
+
+print(5)                   # Print an integer
+print(5.1)                 # Print a floating-point number
+print(date.today())        # Print today's date
