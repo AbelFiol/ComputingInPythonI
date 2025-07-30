6eb5c198d985daf474fde6927b6b7a1841a37a16
AbelFiol <abelfiol2712@gmail.com>
2025-07-30 10:55:52 -0500

    Remove Data Types and Variables 1

--- DataTypesandVariables-1.py
+++
@@ -1,5 +0,0 @@
-from datetime import date  # Import the date class from the datetime module
-
-print(5)                   # Print an integer
-print(5.1)                 # Print a floating-point number
-print(date.today())        # Print today's date
